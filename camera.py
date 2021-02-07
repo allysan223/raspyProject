4f56a3e4fbf582e1394f8e2019b284861a9ee80b
allysan223 <allysan@uw.edu>
2021-02-07 15:04:26 -0800

    picamera stream

--- camera.py
+++ camera.py
@@ -1,8 +1,13 @@
 from picamera import PiCamera
 from time import sleep
+import sys
 
 camera = PiCamera()
 
+#Start camera preview
 camera.start_preview()
+#Sleep for atealst 2 seconds to allow sensors to sense light exposure
 sleep(5)
+#take photo
+camera.capture('/home/pi/raspyProject/pics/image.jpg')
 camera.stop_preview()
