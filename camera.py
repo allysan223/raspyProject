d258f6f7a5154c2439525f11241074a983049701
allysan223 <allysan@uw.edu>
2021-02-20 14:06:22 -0800

    set up camera

--- camera.py
+++ camera.py
@@ -2,12 +2,12 @@ from picamera import PiCamera
 from time import sleep
 import sys
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import printer
 
 def button_callback(channel): #callback will run in another thread when event detected
     print("Button was pushed!")
     takePhoto()
-    print("Photo saved
-    !")
+    print("Photo saved!")
 
 def takePhoto():
     #Start camera preview
@@ -17,18 +17,23 @@ def takePhoto():
     #take photo
     camera.capture('/home/pi/raspyProject/pics/image.jpg')
     camera.stop_preview()
+    printer.preparePhoto()
+    printer.printPhoto()
 
 #Initialize Button
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-#Set up button push event
+#Set up button push event, regardless of whatever else is happening in the program, the function my_callback will be run
 GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
 
 #Initialize camera
 camera = PiCamera()
 
-while True:
-    continue
-
-GPIO.cleanup() # Clean up
+# do nothing while waiting for button to be pushed
+try:  
+    while True:
+        continue
+except KeyboardInterrupt:  
+    GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
+GPIO.cleanup() 
--- printer.py
+++ printer.py
@@ -1,16 +1,33 @@
-from PIL import Image
-from thermalprinter import *
+from escpos import printer
+from escpos.printer import Usb
+import cv2
+ 
+def preparePhoto():
+    img = cv2.imread('pics/image.jpg', cv2.IMREAD_UNCHANGED)
+    print('Original Dimensions : ',img.shape)
+    scale_percent = 26.7 # percent of original size, width max 384. 385 too big
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    
+    # resize image
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    resized = cv2.rotate(resized, cv2.cv2.ROTATE_90_CLOCKWISE) 
+    cv2.imwrite("pics/resize.png", resized) 
+    print('Resized Dimensions : ',resized.shape)
 
-with ThermalPrinter(port='/dev/usb/lp0') as printer:
-    # Picture
-    #printer.image(Image.open('gnu.png'))
+def printPhoto():
+    p = printer.File("/dev/usb/lp0")
+    p.text("Test print")
+    #p.barcode('1324354657687', 'EAN13', 64, 2, '', '')
+    print("barcode")
+    #p.image("pics/house.jpg")
+    p.image("pics/resize.png")
 
-    # Bar codes
-    printer.barcode_height(80)
-    printer.barcode_position(BarCodePosition.BELOW)
-    printer.barcode_width(3)
-    printer.barcode('012345678901', BarCode.EAN13)
+    print("image printed")
+    #p.cut()
 
 
-    # Line feeds
-    printer.feed(2)
+# with open('/dev/usb/lp0', 'wb') as printer:
+#     printer.write("1234567890123456789012345678901234567890\n")
+#     printer.write("Line 2\n")
