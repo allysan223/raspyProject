0d8fc2ce9dac5f78937e4c4ae7515459bc960b53
allysan223 <allysan@uw.edu>
2021-03-02 21:02:01 -0800

    LED set up

--- camera.py
+++ camera.py
@@ -20,12 +20,16 @@ def takePhoto():
     printer.preparePhoto()
     printer.printPhoto()
 
-#Initialize Button
+#Initialize GPIO pins
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+#Initialize Button
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #PUD_DOWN# Set pin 10 to be an input pin and set initial value to be pulled low (off)
 #Set up button push event, regardless of whatever else is happening in the program, the function my_callback will be run
 GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+#Initialize boot-up LED
+GPIO.setup(12,GPIO.OUT)
+GPIO.output(12,GPIO.HIGH)
 
 #Initialize camera
 camera = PiCamera()
@@ -35,5 +39,9 @@ try:
     while True:
         continue
 except KeyboardInterrupt:  
-    GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
-GPIO.cleanup() 
+    GPIO.cleanup()       # clean up GPIO on CTRL+C exit
+    GPIO.output(12,GPIO.LOW)  
+    
+GPIO.output(12,GPIO.LOW)   
+GPIO.cleanup()
+
